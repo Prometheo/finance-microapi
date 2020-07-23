006c1a74b5fbe8ac1dc0978ede38334490807aef
Prometheo <olaoyejnr@gmail.com>
2020-07-23 19:53:49 +0100

    added a function to load data into the DB

--- currency_converter/management/commands/initadmin.py
+++ currency_converter/management/commands/initadmin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
+
 class Command(BaseCommand):
 
     help = 'Create super user'
@@ -23,3 +24,9 @@ class Command(BaseCommand):
                 User.objects.create_superuser(username=username, email='', password='user')
             else:
                 User.objects.create_user(username=username, email='', password='user')
+
+
+            
+
+
+    
